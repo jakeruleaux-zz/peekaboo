ef1be2f33d26fb9fd48c512d7c69234eff4c88b7
jacob ruleaux and rena rallis <jacob@>
2017-05-30 16:37:14 -0700

    add fadeout command

--- js/scripts.js
+++ js/scripts.js
@@ -3,4 +3,7 @@ $(document).ready(function() {
     $(".walrus-showing").toggle();
     $(".walrus-hidden").toggle();
   });
+  $(".clickHere").click(function() {
+    $(".panda-fadeOut").fadeOut();
+  });
 });
